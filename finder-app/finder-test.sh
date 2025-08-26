676ef8378c7aa638f20a7799120a539f2052efa6
holmberg18 <holmberg18@Ubuntu.myguest.virtualbox.org>
2025-08-26 16:33:18 -0700

    assignment-4-part-2: Update Makefile, finder-test.sh, finder.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
+# Modified for Assignment 4 Buildroot integration
+
 
 set -e
 set -u
 
+
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+
+# Read username from the correct location (installed by Buildroot)
+username=$(cat /etc/finder-app/conf/username.txt)
+
+
+# Use the cross-compiled applications installed by Buildroot
+WRITER_APP="writer"
+FINDER_SCRIPT="finder.sh"
+
 
 if [ $# -lt 3 ]
 then
@@ -25,43 +37,54 @@ else
 	WRITEDIR=/tmp/aeld-data/$3
 fi
 
+
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
+
+# Remove any previous build artifacts (don't try to build natively)
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
 
-if [ $assignment != 'assignment1' ]
+# Create the write directory
+mkdir -p "$WRITEDIR"
+if [ -d "$WRITEDIR" ]
 then
-	mkdir -p "$WRITEDIR"
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+	echo "$WRITEDIR created"
+else
+	echo "Failed to create $WRITEDIR"
+	exit 1
 fi
 
-# Use compiled writer instead of writer.sh
+
+# Use the cross-compiled writer utility from PATH
 for i in $(seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	$WRITER_APP "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
-# remove temporary directories
+# Use the finder script from PATH
+OUTPUTSTRING=$($FINDER_SCRIPT "$WRITEDIR" "$WRITESTR")
+
+
+# Write result to file for assignment 4 requirement
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
+echo "Result written to /tmp/assignment4-result.txt"
+
+
+# Clean up
 rm -rf /tmp/aeld-data
 
+
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
